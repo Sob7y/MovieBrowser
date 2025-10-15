832a1aa38ab40aa937c06d99e73f44b440790d3b
sobhy <mohamed.sobhy@squadio.com>
2025-10-15 13:24:21 +0300

    Initial Commit

---
+++ MovieBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieBrowser
+//
+//  Created by Mohamed Khaled on 15/10/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
